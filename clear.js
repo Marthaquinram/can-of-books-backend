6a488b4bd05d91c3e425900c56254f669cb98039
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-06-04 15:54:15 -0700

    commented out my clear.js

--- clear.js
+++ clear.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_CONNECT);
+// mongoose.connect(process.env.MONGODB_CONNECT);
 
-const Book = require('./models/book');
+// const Book = require('./models/book');
 
-async function clear() {
-  try {
-    await Book.deleteMany({});
-    console.log('Book clear');
-  } catch(err) {
-    console.error(err);
-  } finally {
-    mongoose.disconnect();
-  }
-}
+// async function clear() {
+//   try {
+//     await Book.deleteMany({});
+//     console.log('Book clear');
+//   } catch(err) {
+//     console.error(err);
+//   } finally {
+//     mongoose.disconnect();
+//   }
+// }
 
-clear();
+// clear();
