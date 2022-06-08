9bf6db7846652135613d32709a0ddd538cccf330
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-06-08 00:49:56 -0400

    Lab 12 ready for canvas submission

--- modules/handlers.js
+++ modules/handlers.js
@@ -12,4 +12,16 @@ async function getBooks(request, response, next) {
   }
 }
 
-module.exports = getBooks;
+const deleteBook = async (request, response, next) => {
+  try {
+    await BookModel.findByIdAndDelete(request.params.id);
+    response.status(204);
+
+  } catch (error) {
+    error.customMessage = 'Something went wrong with deleting your book!'
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
+
+module.exports = { getBooks, deleteBook };
