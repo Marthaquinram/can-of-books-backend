f3734130eb4dc8b957dcd5eea8a479cdd6d642c0
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-06-13 15:25:42 -0700

    completed lab 15

--- models/book.js
+++ models/book.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: {type: String, required: true},
   description: {type: String, required: true},
-  status: {type: String, enum: ['My Personal Favs', 'In your Feels', 'CHILLING STORIES', 'Healing yourself']},
+  status: { type: String, enum: ['My Personal Favs', 'In your Feels', 'CHILLING STORIES', 'Healing yourself'] },
+  email:{type: String, required: true} 
 });
 
 // use schema to craft a book model
